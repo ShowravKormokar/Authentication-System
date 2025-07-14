27c6635cc6180e84c4a4154768464463bb31c475
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-14 23:30:10 +0600

    Setup cors policies to accept request only specified origin

--- server-side/server.js
+++ server-side/server.js
@@ -7,6 +7,15 @@ require('./db/connection');
 
 const app = express();
 
+// Setup CORS to allow request from frontend/client-side
+const corsOptions = {
+    origin: "http://localhost:3000/", // Request allow only these origin
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // only allow these HTTP methods,
+    allowedHeaders: ["Content-Type", "Authorization"], // only allow these headers,
+    credential: true, // allow cookies or credentials if needed
+}
+app.use(cors(corsOptions));
+
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

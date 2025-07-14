b5c5d2e42d3b5c936d767507a9d06a43dbc05766
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-14 23:17:41 +0600

    Setup server that successfully running on port

--- server-side/server.js
+++ server-side/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
+
+require('./db/connection');
+
+const app = express();
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
+});

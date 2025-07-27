70f70f37e435e9cd8b41aa7eb442616eb34ac0ec
ShowravKormokar <showravkormokar354@gmail.com>
2025-07-27 20:52:09 +0600

    Design register page

--- client-side/src/app/register/page.tsx
+++ client-side/src/app/register/page.tsx
@@ -55,8 +55,8 @@ const register = () => {
 
     return (
         <>
-            <div className='flex justify-center items-center w-full h-[80vh] rounded-lg bg-gray-50'>
-                <form onSubmit={handleSubmit} className=' bg-white shadow-md rounded p-6 w-full max-w-md'>
+            <div className='flex justify-center items-center w-full h-[80vh] rounded-lg '>
+                <form onSubmit={handleSubmit} className=' bg-white/7 backdrop-blur-[20px] backdrop-saturate-[155%] rounded-xl p-6 w-full max-w-md shadow-[0_0_25px_rgba(6,182,212,0.4)] border-2 border-[#0F172A]'>
                     <h1 className="text-2xl font-bold mb-4 text-center">Register</h1>
                     <input
                         type="text"
@@ -65,7 +65,7 @@ const register = () => {
                         onChange={handleChange}
                         value={registerDatas.userName}
                         required
-                        className='w-full border p-2 rounded mb-4'
+                        className='w-full p-2 rounded mb-4 bg-slate-900 border border-slate-700 text-cyan-50 focus:ring-2 focus:ring-cyan-500'
                     />
                     <input
                         type="email"
@@ -74,7 +74,7 @@ const register = () => {
                         onChange={handleChange}
                         value={registerDatas.email}
                         required
-                        className='w-full border p-2 rounded mb-4'
+                        className='w-full p-2 rounded mb-4 bg-slate-900 border border-slate-700 text-cyan-50 focus:ring-2 focus:ring-cyan-500'
                     />
                     <input
                         type="password"
@@ -83,7 +83,7 @@ const register = () => {
                         onChange={handleChange}
                         value={registerDatas.password}
                         required
-                        className='w-full border p-2 rounded mb-4'
+                        className='w-full p-2 rounded mb-4 bg-slate-900 border border-slate-700 text-cyan-50 focus:ring-2 focus:ring-cyan-500'
                     />
                     <input
                         type="password"
@@ -92,19 +92,19 @@ const register = () => {
                         onChange={handleChange}
                         value={registerDatas.cPassword}
                         required
-                        className='w-full border p-2 rounded mb-4'
+                        className='w-full p-2 rounded mb-4 bg-slate-900 border border-slate-700 text-cyan-50 focus:ring-2 focus:ring-cyan-500'
                     />
                     {/* Error */}
                     {error && <p className="text-red-500 mb-3">{error}</p>}
 
                     <button
                         type="submit"
-                        className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700 cursor-pointer">
+                        className="w-full bg-[#06B6D4] text-white py-2 rounded hover:bg-[#0891B2] hover:shadow-[0_0_10px_rgba(6,182,212,0.7)] cursor-pointer">
                         Register
                     </button>
                     <p className="text-center text-sm text-gray-600 mt-4">
                         Have an account?
-                        <Link href="/login" className="text-green-600 hover:underline ml-1">
+                        <Link href="/login" className="text-[#0EA5E9] hover:underline ml-1">
                             Log In
                         </Link>
                     </p>

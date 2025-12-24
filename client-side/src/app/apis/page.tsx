9152889527b8552f74058915b1f57eba250575db
ShowravKormokar <showravkormokar354@gmail.com>
2025-12-24 22:56:50 +0600

    Make apis page layout

--- client-side/src/app/apis/page.tsx
+++ client-side/src/app/apis/page.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import img01 from "../../assets/images/6846415.jpg"
 
 const apis = () => {
     return (
-        <div>apis</div>
+        <main className='w-full h-full'>
+            <section className='w-screen h-dvh text-center content-center'>
+                <h1 className='text-[11vw]'>Hello, World!</h1>
+            </section>
+            <section className='w-screen border-2 border-amber-500'>
+                <div className="row">
+                    <img src={img01.src} alt="" />
+                </div>
+                <div className="row">
+                    <img src={img01.src} alt="" />
+                </div>
+                <div className="row">
+                    <img src={img01.src} alt="" />
+                </div>
+                <div className="row">
+                    <img src={img01.src} alt="" />
+                </div>
+            </section>
+            <section className='w-screen h-dvh border-2 border-amber-500'>
+
+            </section>
+        </main>
     );
 };
 

832050af8b9467e0b3e5bf96d40233a77bf87b2b
ShowravKormokar <showravkormokar354@gmail.com>
2026-01-01 22:29:52 +0600

    Make hero section layout

--- client-side/src/app/docs/page.tsx
+++ client-side/src/app/docs/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import dh from "../../assets/images/dashboard.png"
 
 const docs = () => {
     return (
-        <div>docs</div>
+        <main>
+            <section className="docs-hero">
+                <canvas></canvas>
+                <div className="docs-hero-content">
+                    <div className="docs-header">
+                        <h1>One unified workspace to build, test, and ship AI faster</h1>
+                        <p>Tursted by</p>
+                    </div>
+                </div>
+                <div className="docs-hero-img">
+                    <div className="dh-img">
+                        <img src={dh.src} alt="hero-dashboard" />
+                    </div>
+                </div>
+            </section>
+
+            <section className="w-full h-dvh"></section>
+        </main>
     )
 }
 
